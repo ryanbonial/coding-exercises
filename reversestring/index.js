5e935eadf7ad464b7ded6e982c073a263ab94be6
Ryan Bonial <ryanbonial@gmail.com>
2020-06-10 11:41:27 -0600

    Reversing strings

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // -- unicode-safe
+  return Array.from(str).reverse().join('');
+
+  // -- Classic
+  return str.split('').reverse().join('');
+
+  // -- without using reverse
+  let revArr = [];
+  Array.from(str).forEach(letter => revArr.unshift(letter));
+  return revArr.join('');
+
+  // -- old-school backwards
+  let revStr = '';
+  for (let xx = str.length - 1; xx >= 0; xx--) {
+    revStr += str[xx];
+  }
+  return revStr;
+}
 
 module.exports = reverse;
--- reversestring/test.js
+++ reversestring/test.js
@@ -8,6 +8,10 @@ test('Reverse reverses a string', () => {
   expect(reverse('abcd')).toEqual('dcba');
 });
 
-test('Reverse reverses a string', () => {
+test('Reverse reverses a string with spaces', () => {
   expect(reverse('  abcd')).toEqual('dcba  ');
+});
+
+test('Reverse reverses a string with unicode', () => {
+  expect(reverse('🚀abcd')).toEqual('dcba🚀');
 });
